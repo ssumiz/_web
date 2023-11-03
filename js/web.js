49842c40e8fe524e7d45fbe61b152cfc0936bd7a
ssumiz <2557636@naver.com>
2023-11-03 17:58:37 +0900

    슬라이드기능추가

--- js/web.js
+++ js/web.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
     //화면이 준비되면 실행해라.
 
+    setInterval(function () {
+        $(".swiper-wrapper").animate({
+            "marginTop": -300
+        }, 1000, function () {
+            $("div:first-child", this).appendTo($(this))
+            $(this).css("marginTop", 0)
+        })
+    }, 3000)
+
+
 })
